7a5b6745da905e66bf07870aadd28f2c265cae5a
Doğukan ÖKSÜZ <oksuz.dogukan@gmail.com>
2025-06-01 00:24:09 +0300

    Added recommended films page, card and fetching selected movie genres

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import genresMap from "../contant/genres"
 
-function FilmCard() {
+function FilmCard({ title , poster, genres, released_date , vote}) {
+
+  const getGenreName = (id) => {
+    return Object.keys(genresMap).find((name) => genresMap[name] === id);
+  }
+
+
+
   return (
-    <div>FilmCard</div>
+    <div className="bg-white text-[#2c3e50] p-6 rounded-xl flex flex-col gap-4 justify-between" >
+      
+      <div className="font-bold text-xl">{title}</div>
+
+      <div> <img src={poster} alt="#" className="rounded-xl" /> </div>
+
+      <div className="flex flex-col gap-1 text-lg">
+        <div>Genres : {genres.map(getGenreName).join(", ")}</div>
+        <div>Released : {released_date}</div>
+        <div>Filmvote : {vote}</div>
+      </div>
+      
+      <div className="bg-[#f0f0f0] font-medium text-center w-[80%] mx-auto p-1 rounded-xl cursor-pointer shadow-[0_5px_15px_rgba(52,152,219,0.3)] hover:transform-[translateY(-3px)] hover:bg-[#d6eaf8] duration-200">Add Watch Later</div>
+
+    </div>
   )
 }
 
--- src/pages/FilmsPage.jsx
+++ src/pages/FilmsPage.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import FilmCard from '../components/FilmCard';
 
 function FilmsPage() {
+  
+  const location = useLocation();
+  const [films, setFilms] = useState([]);
+
+  const API_BASE_URL = "https://api.themoviedb.org/3/discover/movie?&language=en-US";
+  const API_KEY = "e1bc947a2e4d2cc75b47d223ad80b8da"
+
+  const query = new URLSearchParams(location.search)
+  const genreIds = query.get("genres")?.split(",").map(id => parseInt(id))
+
+  const fetchMovies = async (genreIds) => {
+
+    try {
+      const response = await axios.get(API_BASE_URL, {
+      params: {
+        api_key: API_KEY,
+        with_genres: genreIds.join(","),
+        language: "en-US"
+      }      
+    })
+    console.log(response.data.results)  
+    setFilms(response.data.results);
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+
+  useEffect(() => {
+    if (genreIds.length > 0) {
+      fetchMovies(genreIds);
+    }
+  }, [location.search]);
+
+
   return (
-    <div>FilmsPage</div>
+    <main className='grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-10 p-10'>
+      {
+        (films && films.map((film) => {
+          return(
+            <FilmCard 
+            key={film.id}
+            title={film.original_title} 
+            poster={`https://image.tmdb.org/t/p/w500${film.poster_path}`} 
+            genres={film.genre_ids}
+            released_date={film.release_date}
+            vote={film.vote_average}/>
+
+          )
+        }))
+      }
+    
+    
+    </main>
   )
 }
 
